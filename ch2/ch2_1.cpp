fef41c7d464f62f0e4fb6de6f1c168cba2d7e7f0
kmad <kashyap.mad@gmail.com>
2013-03-18 12:36:40 -0700

    office commit

--- ch2/ch2_1.cpp
+++ ch2/ch2_1.cpp
@@ -11,6 +11,19 @@ int main()
 	std::cin >> name;
 
 	const std::string greeting = "Hello, " + name + "!";
+	const int pad = 1;
+	const int rows = pad * 2 + 3;
+	std::cout << std::endl;
+	
+	//separate the output from the input
+	int r = 0;
+
+	//invariant: we have writter r rows so far
+	while(r != rows){
+		std::cout << std::endl;
+		++r;
+	}
+
 	std::cout << greeting;
 	return 0;
 }
