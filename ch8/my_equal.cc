e557b582c2f507847b1340bb514a777957a7f33e
Kashyap Maduri <kashyapmaduri@outlook.com>
2014-11-24 16:14:02 -0800

    revised git ignore. ignore everything except dirs source files

---
+++ ch8/my_equal.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+template <class In, class Rev>
+bool my_equal(In b, In e, Rev f)
+{
+    while(b != e) {
+        if(*b++ != *f++) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isPalindrome(const string& s)
+{
+    return my_equal(s.begin(), s.end(), s.rbegin());
+}
+
+int main()
+{
+    vector<string> words;
+    words.push_back("palindrome");
+    words.push_back("madam");
+    words.push_back("frooti");
+    words.push_back("abcdedcba");
+
+    for(vector<string>::size_type i = 0; i < words.size(); i++) {
+        cout << "Is \"" << words[i] << "\" a palindrome? ";
+        if(isPalindrome(words[i])) {
+            cout << "yes ";
+        } else {
+            cout << "no ";
+        }
+        cout << endl;
+    }
+    
+}
