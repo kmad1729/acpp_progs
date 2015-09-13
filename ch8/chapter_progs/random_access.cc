7e6d071154f8b36065cbfd4560ba7b3ac862b005
Kashyap Maduri <kashyapmaduri@outlook.com>
2015-09-12 22:29:18 -0700

    added template for binary search

--- ch8/chapter_progs/random_access.cc
+++ ch8/chapter_progs/random_access.cc
@@ -7,6 +7,7 @@
 #include <ctime>
 #include <algorithm>
 #include <unistd.h>
+#include <string>
 #define MAX 100
 
 
@@ -17,6 +18,7 @@ using std::rand;
 using std::srand;
 using std::time;
 using std::generate;
+using std::string;
 using std::binary_search;
 //check whether a number exists in a vector of strings
 
@@ -24,6 +26,22 @@ using std::binary_search;
 int get_rand()
 {
     return rand() % MAX;
+}
+
+
+template <class In, class X>
+bool my_binary_seach(In beg, In end, X tgt)
+{
+    while(beg < end) {
+        In mid = beg + (end - beg) / 2;
+        if(tgt < *mid) 
+            end = mid;
+        else if (tgt > *mid)
+            beg = mid + 1;
+        else 
+            return true;
+    }
+    return false;
 }
 
 //generate a randome vector of given size
@@ -45,12 +63,15 @@ int main()
         else cout << ", " << v[i];
     }
     cout << "}" << endl;
+    string delim = string(20, '*') + "\n";
 
     vector<int> tgts;
     tgts.push_back(23);
     tgts.push_back(52);
     tgts.push_back(42);
     tgts.push_back(2);
+    cout << delim;
+    cout << "with stl binary search -->" << endl;
     for(vector<int>::size_type i = 0; i < tgts.size(); i++) {
         if(binary_search(v.begin(), v.end(), tgts[i])) {
             cout << tgts[i] << " was found in the vector" << endl;
@@ -58,5 +79,17 @@ int main()
             cout << tgts[i] << " was not found in the vector" << endl;
         }
     }
+    cout << delim;
+
+    cout << "with my binary search -->" << endl;
+    for(vector<int>::size_type i = 0; i < tgts.size(); i++) {
+        if(my_binary_seach(v.begin(), v.end(), tgts[i])) {
+            cout << tgts[i] << " was found in the vector" << endl;
+        } else {
+            cout << tgts[i] << " was not found in the vector" << endl;
+        }
+    }
+    cout << delim;
+
     return 0;
 }
