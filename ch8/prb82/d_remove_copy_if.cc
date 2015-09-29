bf8312c3cc56885478ba5e64e9898a2917d1b36f
Kashyap Maduri <kashyapmaduri@outlook.com>
2015-09-29 09:06:03 -0700

    added my implementation of remove_copy_if

--- ch8/prb82/d_remove_copy_if.cc
+++ ch8/prb82/d_remove_copy_if.cc
@@ -2,11 +2,28 @@
 
 #include "my_include.h"
 
+
+template <class ForwardIterator, class OutputIterator, class Predicate>
+OutputIterator my_remove_copy_if(ForwardIterator b, ForwardIterator e, OutputIterator result, Predicate pred)
+{
+    while(b != e) {
+        if(!pred(*b)) {
+            *result++ = *b;
+        }
+        b++;
+    }
+    return result;
+}
+
 bool is_5_multiple (int i)
 {
     return (i % 5 == 0);
 }
 
+bool is_3_multiple (int i)
+{
+    return (i % 3 == 0);
+}
 
 int main()
 {
@@ -24,6 +41,15 @@ int main()
 
     cout << "non multiples of 5 in the vector v -->" << endl;
     print_vector_to_out(non_five_multiples);
+    cout << delim;
+
+    cout << "removing multiples of 3" << endl;
+
+    vector<int> non_three_multiples;
+    my_remove_copy_if(v.begin(), v.end(), back_inserter(non_three_multiples), is_3_multiple);
+
+    cout << "non multiples of 3 in the vector v -->" << endl;
+    print_vector_to_out(non_three_multiples);
     cout << delim;
 
 }
