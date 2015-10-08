a5d7ee1d8e9b6c765853bdc1160127f99ae3f904
Kashyap Maduri <kashyapmaduri@outlook.com>
2015-10-08 16:36:00 -0700

    implemented xref. Added the question

--- ch8/prb85/xref_impl.cc
+++ ch8/prb85/xref_impl.cc
@@ -11,6 +11,7 @@ using std::vector;
 using std::cout;
 using std::cin;
 using std::endl;
+using std::istream;
 using std::string;
 using std::getline;
 using std::ostream_iterator;
@@ -53,18 +54,40 @@ void print_vector(const vector<T>& v)
     cout << "}" << endl;
 }
 
+map<string, vector<int> > xref_impl(istream& in, vector<string> find_words(const string&) = split)
+{
+    string line;
+    map<string, vector<int> > result;
+    int line_number = 0;
+
+    while(getline(in, line)) {
+        ++line_number;
+        vector<string> tokens = find_words(line);
+        for(vector<string>::const_iterator it = tokens.begin();
+                it != tokens.end(); it++) {
+            result[*it].push_back(line_number);
+        }
+    }
+    return result;
+
+}
+
 int main()
 {
     string line;
     string delim = string(20, '*') + '\n';
 
-    while(getline(cin, line)) {
-        cout << "line --> " << endl;
-        cout << line << endl << delim;
-        cout << "split vector --> " << endl;
-        print_vector(split(line));
+    map<string, vector<int> > m = xref_impl(cin);
+
+    for(map<string, vector<int> >::const_iterator it = m.begin();
+            it != m.end(); it++) {
+        cout << "\"" << (it -> first) << "\" occurs at ";
+        vector<int> occurences = (it -> second);
+        copy(occurences.begin(), occurences.end(), 
+                ostream_iterator<int>(cout, " "));
         cout << endl;
     }
+
     return 0;
 }
 
