17a0307b458717f05a5b47ff7ecc40e6d71b5c11
Kashyap Maduri <kashyapmaduri@outlook.com>
2015-11-06 23:43:01 -0800

    adding code for basic pointer logic

--- ch10/basic_ptr.cc
+++ ch10/basic_ptr.cc
@@ -3,14 +3,13 @@
 using std::cout;
 using std::endl;
 
+
 int main()
 {
-    int x = 5;
-    int *p;
-    p = &x;
+    int x = 42;
+    int *p = &x;
 
     cout << "x = " << x << endl;
-    *p = 6;
+    *p += 1;
     cout << "x = " << x << endl;
-    return 0;
 }
