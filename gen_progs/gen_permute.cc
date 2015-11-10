0ffd50ca65e21279bced01403c85ff360d6f1e9e
Kashyap Maduri <kashyapmaduri@outlook.com>
2015-11-09 18:57:14 -0800

    revised permute function

--- gen_progs/gen_permute.cc
+++ gen_progs/gen_permute.cc
@@ -7,15 +7,30 @@
 
 using std::endl;
 using std::cout;
+using std::string;
+using std::swap;
 
 template <class Ran>
-void permute(Ran start_index, Ran b, Ran e)
+void permute(Ran b, Ran e, Ran start_index)
 {
+    if (start_index == e) {
+        for(Ran i = b; i != e; i++) {
+            cout << *i;
+        }
+        cout << endl;
+    } else {
+        for(Ran i = start_index; i != e; i++) {
+            swap(*i, *start_index);
+            permute(b, e, start_index + 1);
+            swap(*i, *start_index);
+        }
+    }
+
 }
 
 int main()
 {
     string s = "abc";
     cout << "permutation of (" << s << ") -->" << endl;
-    permute(s.begin(), s.begin(), s.end());
+    permute(s.begin(), s.end(), s.begin());
 }
