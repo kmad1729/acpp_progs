8b29ca9818b531197ecbadf33c6f66779978c1e2
Kashyap Maduri <kashyapmaduri@outlook.com>
2015-11-24 23:24:38 -0800

    adding a demo to static pointer

---
+++ ch10/pointer_to_static.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int* pointer_to_static()
+{
+    static int x;
+    return &x;
+}
+
+
+int main()
+{
+    int *x = pointer_to_static();
+    int *y = pointer_to_static();
+    cout << "setting *x to 42 " << endl;
+    *x = 42;
+    cout << "*x = " << *x << " *y = " << *y << endl;
+    cout << "setting *y to 1729" << endl;
+    *y = 1729;
+    cout << "*x = " << *x << " *y = " << *y << endl;
+    cout << "setting z to pointer_to_static" << endl;
+    int *z = pointer_to_static();
+    cout << "*z = " << *z << endl;
+}
