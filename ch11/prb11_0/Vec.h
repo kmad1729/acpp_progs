42c40a03b0967ec47f78704e06c527ed02df03b3
Kashyap Maduri <kashyapmaduri@outlook.com>
2015-12-04 23:28:35 -0800

    starting chapter problems from chapter 11

---
+++ ch11/prb11_0/Vec.h
@@ -0,0 +1,13 @@
+#ifndef Guard_Vec_h
+#define Guard_Vec_h
+
+template <class T>
+class Vec {
+    public:
+        typedef T value_type;
+        typedef T& reference_type;
+        typedef const T& const_referece_type;
+        typedef T* iterator;
+};
+
+#endif
