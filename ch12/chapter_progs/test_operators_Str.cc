4f52eb89d062f8edea05bf8e7edade68be6faec4
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-01-13 22:06:34 -0800

    adding a test for >> operator on a tmp variable to read a stream

--- ch12/chapter_progs/test_operators_Str.cc
+++ ch12/chapter_progs/test_operators_Str.cc
@@ -24,8 +24,25 @@ int main() {
     cin >> name;
     cout << "Hello, " << name << "!" << endl;
 
-    cout << "over-writing name varialble " << endl;
+    cout << "over-writing name variable " << endl;
     cout << "enter your frineds name: ";
     cin >> name;
     cout << "hello to " << name << " too!" << endl;
+
+
+    cout << delim << endl;
+
+    cout << "Please enter a long string with spaces for one more test. " <<
+        "I will print out each word in a new line: ";
+
+    Str tmp;
+    Vec<Str> v;
+    while(cin >> tmp)
+        v.push_back(tmp);
+
+    cout << endl;
+    for(Str::size_type i = 0; i < v.size(); i++)
+        cout << v[i] << endl;
+
+    cout << delim << endl;
 }
