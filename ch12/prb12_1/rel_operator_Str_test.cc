4261ae70e2fe8c813475309c813ce867ce4a6833
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-01-18 23:16:54 -0800

    done testing == operator

--- ch12/prb12_1/rel_operator_Str_test.cc
+++ ch12/prb12_1/rel_operator_Str_test.cc
@@ -12,23 +12,41 @@ int main()
 {
     Str delim(20, '*');
     Str s1("apple");
-    char* c_str1 = "apple";
+    char c_str1[] = "apple";
     Str s2("mango");
 
     cout << "s1 = (" << s1 << ")" << endl;
     cout << "s2 = (" << s2 << ")" << endl;
     cout << delim << endl;
 
-    cout << "testing == rel operator" << endl;
+    cout << "testing (==) rel operator" << endl;
     if(s1 == s2) 
-        cout << s1 << " == " << s2 << "? " << "true";
+        cout << s1 << " == " << s2 << " ? " << "true";
     else
-        cout << s1 << " == " << s2 << "? " << "false";
+        cout << s1 << " == " << s2 << " ? " << "false";
+
+    cout << endl;
 
     if(s1 == s1) 
-        cout << s1 << " == " << s1 << "? " << "true";
+        cout << s1 << " == " << s1 << " ? " << "true";
     else
-        cout << s1 << " == " << s1 << "? " << "false";
+        cout << s1 << " == " << s1 << " ? " << "false";
 
+    cout << endl;
+    cout << delim << endl;
+
+    cout << "testing (==) rel operator between Str and const char *" << endl;
+    if(c_str1 == s1) 
+        cout << c_str1 << " == " << s1 << " ? " << "true";
+    else
+        cout << c_str1 << " == " << s1 << " ? " << "false";
+
+    cout << endl;
+    if(c_str1 == s2) 
+        cout << c_str1 << " == " << s2 << " ? " << "true";
+    else
+        cout << c_str1 << " == " << s2 << " ? " << "false";
+
+    cout << endl;
     cout << delim << endl;
 }
