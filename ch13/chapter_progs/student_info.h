873d801e13eee944e834e9bdd166f581938bc7ab
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-02-05 21:42:28 -0800

    adding a function to calculate number of ones recursively

--- ch13/chapter_progs/student_info.h
+++ ch13/chapter_progs/student_info.h
@@ -1,11 +1,30 @@
 #pragma once
 
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
 class Core {
     public:
+        Core();
+        Core(std::istream&);
+        std::istream& read(std::istream&);
+        std::string name() const {return n;}
 
-    private
+    protected:
+        std::istream& read_common(std::istream&);
+        double midterm, final;
+        std::vector<double> homework;
+
+    private:
+        std::string n;
 };
 
 
 class Grad: public Core {
+    public:
+        Grad();
+        Grad(std::istream&);
+        std::istream& read
 };
