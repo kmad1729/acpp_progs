288089fcd6e803af63696f5ec5b83cab9b6e9f63
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-05-28 23:27:06 -0700

    fixed the median function

--- ch13/chapter_progs/till_13_4/median.cc
+++ ch13/chapter_progs/till_13_4/median.cc
@@ -11,6 +11,7 @@ double median(vector<double> v)
     if(v.size() == 0)
         throw domain_error("median of empty vector cannot be found!");
     typedef vector<double>::size_type vec_sz;
+    sort(v.begin(), v.end());
     vec_sz size = v.size();
     vec_sz mid = size / 2;
     return size % 2 == 0? (v[mid - 1] + v[mid]) / 2: v[mid];
